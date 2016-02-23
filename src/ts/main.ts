4cb0002253fe08d4e3e188b725a87cf60db746db
Dylan Owen <dyltotheo@gmail.com>
2016-02-23 15:47:51 -0800

    Updated the html to hide the view depending on what we're doing. Also fixed gitignore

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,19 +3,28 @@
 /// <reference path="UrlEncoderDecoder.ts" />
 
 function ready(): void {
+    var currentElement: HTMLElement;
+
     var urlObject: Object = UrlEncoderDecoder.decodeUrl();
+
     if (urlObject != null) {
+        currentElement = document.getElementById('science-viewer');
+
         var output: HTMLElement = document.getElementById('science-text');
         var scienceObject: ScienceObject = <ScienceObject>urlObject;
 
         new ScienceViewer(output, scienceObject);
     }
     else {
+        currentElement = document.getElementById('science-maker')
+
         var input: HTMLInputElement = <HTMLInputElement>document.getElementById('science-input');
         var output: HTMLElement = document.getElementById('science-link');
 
         new ScienceMaker(input, output);
     }
+
+    currentElement.style.display = 'inherit';
 }
 
 if (document.readyState != 'loading') {
